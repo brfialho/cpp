ac1d5b834d5c008309cc06f2027ea5e6c278119c
Bruno <brunolegal3@gmail.com>
2026-06-09 21:20:05 -0300

    module00: ex00: megaphone finished

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,13 +6,24 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 20:53:03 by brfialho          #+#    #+#             */
-/*   Updated: 2026/06/09 20:53:04 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/06/09 21:18:16 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-int main(void)
+
+int main(int argc, char **argv)
 {
-	std::cout << "Hello World!\n";
+	if (argc < 2)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
+	for (int i = 1; i < argc; i++)
+	{
+		int j = -1;
+		while (argv[i][++j])
+			std::cout << (char)toupper(argv[i][j]);
+		std::cout << ' ';
+	}
+	std::cout << '\n';
 }
