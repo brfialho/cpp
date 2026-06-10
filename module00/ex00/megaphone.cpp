0c4adf868b0e8249be219943408561f6362b11a5
Bruno <brunolegal3@gmail.com>
2026-06-09 21:39:25 -0300

    module00: ex00: changed megaphone to be more c++ish, though creating obj every loop is very not performative, the nested loop without {} is to eye pleasing

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,24 +6,20 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/09 20:53:03 by brfialho          #+#    #+#             */
-/*   Updated: 2026/06/09 21:18:16 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/06/09 21:35:59 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
-
+#include <string>
 
 int main(int argc, char **argv)
 {
 	if (argc < 2)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n", 0);
 	for (int i = 1; i < argc; i++)
-	{
-		int j = -1;
-		while (argv[i][++j])
-			std::cout << (char)toupper(argv[i][j]);
-		std::cout << ' ';
-	}
+		for (size_t j = 0; j < std::string(argv[i]).length(); j++)
+			std::cout << (char)std::toupper(argv[i][j]);
 	std::cout << '\n';
 }
