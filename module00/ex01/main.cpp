f7f7983bb5ef0044dc031d533ef592d174fca702
brfialho <brunolegal3@gmail.com>
2026-07-07 01:42:17 -0300

    module00: ex01: doing some studying on the c++ language

--- module00/ex01/main.cpp
+++ module00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 23:45:38 by brfialho          #+#    #+#             */
-/*   Updated: 2026/06/11 23:56:07 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/07/07 00:38:10 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,26 @@
 
 int main(void)
 {
+    // std::string input;
+    // std::cin >> input;
+    // std::cout << input << '\n';
+    // std::cout << input;
     std::string input;
-    std::cin >> input;
-    std::cout << input;
+    // // char input;
+    // double f = 1.2324352123;
+    // std::cout << f << '\n';
+    // bool teste = 0;
+    // std::cout << std::boolalpha;
+    // std::cout << std::noboolalpha;
+
+    
+    // std::cout << teste << '\n';
+    while (1)
+    {
+        std::cout << "Type something NOW\n";
+        std::cin >> input;
+        std::cout << "You typed >> " << input << '\n';
+        std::cin.ignore(10000, '\n');
+    }
     return (0);
 }
