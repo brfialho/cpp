cd5f15a191f741adbc7e8c21d04f569a748d0e33
Bruno <brunolegal3@gmail.com>
2026-07-16 17:10:34 -0300

    cpp00: ex02: wip

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,84 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 16:02:28 by brfialho          #+#    #+#             */
-/*   Updated: 2026/07/16 16:02:29 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/07/16 17:10:19 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Account.hpp"
+#include <chrono>
+
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+Account::Account( void )
+{
+	_accountIndex = _nbAccounts;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;;
+
+	_nbAccounts++;
+}
+
+Account::Account( int initial_deposit )
+{
+	_accountIndex = _nbAccounts;
+	_amount = initial_deposit;
+	_nbDeposits = 1;
+	_nbWithdrawals = 0;;
+
+	_nbAccounts++;
+	_totalNbDeposits++;
+	_totalAmount += initial_deposit;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	_amount += deposit;
+	_nbDeposits++;
+
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	if (withdrawal > _amount)
+		return (false);
+
+	_amount -= withdrawal;
+	_nbWithdrawals++;
+
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	return (true);
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
+
+static void	_displayTimestamp( void )
+{
+	
+}
 
