b9361739035afe0327c977075f2fe76d3ea06818
Bruno <brunolegal3@gmail.com>
2026-07-20 18:39:12 -0300

    cpp00: ex02: now display mimics original log (super bad design this shit is terrible with are 42 thinking)

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,13 +6,16 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 16:02:28 by brfialho          #+#    #+#             */
-/*   Updated: 2026/07/16 17:25:47 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/07/20 18:37:47 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
-// #include <chrono>
 #include <iostream>
+// #include <chrono>
+
+#define PURPLE "\033[95m"
+#define RESET "\033[0m"
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -47,39 +50,77 @@ Account::Account( void )
 	_nbWithdrawals = 0;;
 
 	_nbAccounts++;
+
+	_displayTimestamp();
+	std::cout << "created\n";
 }
 
 Account::Account( int initial_deposit )
 {
 	_accountIndex = _nbAccounts;
 	_amount = initial_deposit;
-	_nbDeposits = 1;
-	_nbWithdrawals = 0;;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 
 	_nbAccounts++;
-	_totalNbDeposits++;
 	_totalAmount += initial_deposit;
+
+	_displayTimestamp();
+	std::cout << "index:" <<_accountIndex
+			<< ";amount:" << _amount
+			<< ";created\n";
 }
+
+Account::~Account( void )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			<< ";amount:" << _amount
+			<< ";closed\n";
+}
+
 
 void	Account::makeDeposit( int deposit )
 {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			<< ";p_amount:" << _amount;
+
 	_amount += deposit;
 	_nbDeposits++;
 
 	_totalAmount += deposit;
 	_totalNbDeposits++;
+
+	std::cout << ";deposit:" << deposit
+			<< ";amount:" << _amount
+			<< ";nb_deposits:" << _nbDeposits
+			<< '\n';
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+			<< ";p_amount:" << _amount
+			<< ";withdrawal:";
+	
 	if (withdrawal > _amount)
+	{
+		std::cout << "refused\n";
 		return (false);
+	}
 
 	_amount -= withdrawal;
 	_nbWithdrawals++;
 
 	_totalAmount -= withdrawal;
 	_totalNbWithdrawals++;
+
+	std::cout << withdrawal
+			<< ";amount:" << _amount
+			<< ";nb_withdrawals:" << _nbWithdrawals
+			<< '\n';
 	return (true);
 }
 
@@ -90,20 +131,25 @@ int		Account::checkAmount( void ) const
 
 void	Account::_displayTimestamp( void )
 {
-	std::cout << "14/03/2001 ";
+	std::cout << "[19920104_091532] ";
 }
 
 void	Account::displayStatus( void ) const
 {
 	_displayTimestamp();
-	std::cout << "confia\n";
+	std::cout << "index:" << _accountIndex
+				<< ";amount:" << _amount
+				<< ";deposits:" << _nbDeposits
+				<< ";withdrawals:" << _nbWithdrawals
+				<< "\n";
 }
 
 void	Account::displayAccountsInfos( void )
 {
 	_displayTimestamp();
-	std::cout << "ALL INFO\n";
-}
-
-Account::~Account( void ){
+	std::cout << "accounts:" << _nbAccounts
+				<< ";total:" << _totalAmount
+				<< ";deposits:" << _totalNbDeposits
+				<< ";withdrawals:" << _totalNbWithdrawals
+				<< '\n';
 }
