a46ef3134abba8301ad7b986309f68d38d557e93
Bruno <brunolegal3@gmail.com>
2026-07-25 16:50:55 -0300

    cpp01: ex01: setup

---
+++ cpp01/ex01/Zombie.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/25 14:19:49 by brfialho          #+#    #+#             */
+/*   Updated: 2026/07/25 16:26:55 by brfialho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	ZOMBIE_H
+ #define ZOMBIE_H
+
+#include <string>
+#include <iostream>
+
+class Zombie {
+
+
+public:
+	Zombie();
+	Zombie(std::string name);
+
+	~Zombie();
+
+	void announce( void );
+
+
+private:
+	std::string	name;
+};
+
+#endif
