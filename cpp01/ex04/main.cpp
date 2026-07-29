31ed5129474c8f2a21904b33961be4c6dda8350a
Bruno <brunolegal3@gmail.com>
2026-07-29 20:32:29 -0300

    cpp01: ex04: replace funtion wip

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/28 17:35:52 by brfialho          #+#    #+#             */
-/*   Updated: 2026/07/29 19:57:29 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/07/29 20:30:35 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #include <iostream>
 #include <string>
 
-bool	openFiles(std::ifstream &input, std::ofstream &output, std::string &filename);
+bool		openFiles(std::ifstream &input, std::ofstream &output, std::string &filename);
+std::string	&replace(std::string &buffer, const std::string &oldString, const std::string &newString);
 // void	copyFile(std::ifstream	&input, std::ofstream &output);
 // void	replaceStrings(std::ofstream &output, std::string &oldString);
 
@@ -23,11 +24,11 @@ int main(int argc, char **argv)
 	if (argc != 4)
 		return (std::cout << "Usage:   <filename> <stringToBeReplaced> <replacementString>\n", 1);
 
-	std::string		filename(argv[1]);
-	std::string		oldString(argv[2]);
-	std::string		newString(argv[3]);
-	std::ifstream	input;
-	std::ofstream	output;
+	const std::string	oldString(argv[2]);
+	const std::string	newString(argv[3]);
+	std::string			filename(argv[1]);
+	std::ifstream		input;
+	std::ofstream		output;
 
 	if (filename.empty() || oldString.empty() || newString.empty())
 		return (std::cout << "Usage:   <filename> <stringToBeReplaced> <replacementString>\n", 2);
@@ -37,7 +38,7 @@ int main(int argc, char **argv)
 	std::string	buffer;
 
 	while (std::getline(input, buffer))
-		output << buffer << '\n';
+		output << replace(buffer, oldString, newString) << '\n';
 }
 
 bool	openFiles(std::ifstream &input, std::ofstream &output, std::string &filename)
@@ -51,10 +52,22 @@ bool	openFiles(std::ifstream &input, std::ofstream &output, std::string &filenam
 	return (true);
 }
 
-// void	copyFile(std::ifstream	&input, std::ofstream &output)
-// {
-// 	std::string	buffer;
+std::string	&replace(std::string &buffer, const std::string &oldString, const std::string &newString)
+{
+	static	size_t newLen = newString.length();
+	static	size_t oldLen = oldString.length();
+	std::string	copy(buffer);
+	size_t		pos = 0;
 
-// 	while (std::getline(input, buffer))
-// 		output << buffer << '\n';
-// }
+	while ((pos = buffer.find(oldString, pos)) != (size_t)-1)
+	{
+		// std::cout << buffer.c_str() + pos << ' ' << pos << '\n';
+		if (newLen > oldLen)
+			buffer.resize(newLen - oldLen, newString[0] + 1);
+		for (int i = 0; i < newLen; i++)
+			buffer[pos + i] = newString[i];
+		pos += oldLen;
+	}
+	(void)newString;
+	return (buffer);
+}
