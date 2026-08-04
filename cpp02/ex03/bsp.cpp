85ca45ad57b1c266099817eb29e2f48fa24e2ccc
Bruno <brunolegal3@gmail.com>
2026-08-04 17:26:03 -0300

    cpp02: ex03: linked the files and created main

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,18 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 16:57:05 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/04 16:57:05 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/04 17:24:08 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Point.hpp"
+
+bool	bsp( Point const a, Point const b, Point const c, Point const point)
+{
+	(void)a;
+	(void)b;
+	(void)c;
+	(void)point;
+
+	return (true);
+}
--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -6,14 +6,20 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 19:30:39 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/04 17:03:49 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/04 17:25:42 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Point.hpp"
 
+bool	bsp( Point const a, Point const b, Point const c, Point const point);
+
 int	main( void )
 {
+	Point	a(0, 0), b(1.5, 3), c(3, 0);
+	Point	p(1.5, 1.5);
 
+	std::cout << std::boolalpha;
+	std::cout << bsp(a, b, c, p) << '\n';
 }
