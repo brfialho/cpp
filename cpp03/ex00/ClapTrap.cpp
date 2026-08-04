a7dacf1a022986b2eea22b6610afed53e4bbba91
Bruno <brunolegal3@gmail.com>
2026-08-04 17:49:47 -0300

    cpp03: ex00: setup

---
+++ cpp03/ex00/ClapTrap.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/04 17:48:10 by brfialho          #+#    #+#             */
+/*   Updated: 2026/08/04 17:48:16 by brfialho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{}
+
+ClapTrap::ClapTrap( const ClapTrap &other )
+{}
+
+ClapTrap&	ClapTrap::operator=( const ClapTrap& other )
+{
+
+	return *this;
+}
+
+ClapTrap::~ClapTrap()
+{}
+
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/04 17:48:14 by brfialho          #+#    #+#             */
+/*   Updated: 2026/08/04 17:48:18 by brfialho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+class	ClapTrap {
+
+private:
+
+
+public:
+	ClapTrap();
+	ClapTrap( const ClapTrap &other );
+	ClapTrap& operator=( const ClapTrap &other );
+	~ClapTrap();
+
+};
+
+#endif
