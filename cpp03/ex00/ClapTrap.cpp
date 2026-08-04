9fca9174b7a384a9148c085d647b6660bcc9eada
Bruno <brunolegal3@gmail.com>
2026-08-04 18:14:59 -0300

    cpp03: ex00: constructors

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,24 +6,54 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 17:48:10 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/04 17:48:16 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/04 18:14:50 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap()
-{}
+ClapTrap::ClapTrap():
+_name("UNAMED"),
+_hp(10),
+_mana(10),
+_atk(0)
+{
+	std::cout << "Default Constructor has been called\n";
+}
 
-ClapTrap::ClapTrap( const ClapTrap &other )
-{}
+ClapTrap::ClapTrap( const std::string name ):
+_name(name),
+_hp(10),
+_mana(10),
+_atk(0)
+{
+	std::cout << "Name Constructor has been called\n";
+}
+
+ClapTrap::ClapTrap( const ClapTrap &other ):
+_name(other._name),
+_hp(other._hp),
+_mana(other._mana),
+_atk(other._atk)
+{
+	std::cout << "Copy Constructor has been called\n";
+}
 
 ClapTrap&	ClapTrap::operator=( const ClapTrap& other )
 {
+	std::cout << "Assign Operator has been called\n";
 
+	if (this == &other)
+		return *this;
+
+	this->_name = other._name;
+	this->_hp = other._hp;
+	this->_mana = other._mana;
+	this->_atk = other._atk;
 	return *this;
 }
 
 ClapTrap::~ClapTrap()
-{}
-
+{
+	std::cout << "Destructor has been called\n";
+}
--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,24 +6,34 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 17:48:14 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/04 17:48:18 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/04 18:13:24 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_H
 # define CLAPTRAP_H
 
+#include <string>
+#include <iostream>
+
 class	ClapTrap {
 
 private:
-
+	std::string	_name;
+	int			_hp;
+	int			_mana;
+	int			_atk;
 
 public:
 	ClapTrap();
+	ClapTrap( const std::string name );
 	ClapTrap( const ClapTrap &other );
 	ClapTrap& operator=( const ClapTrap &other );
 	~ClapTrap();
 
+	// void	attack(const std::string& target);
+	// void	takeDamage(unsigned int amount);
+	// void	beRepaired(unsigned int amount);
 };
 
 #endif
