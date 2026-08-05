958fdf759836e664710114881da8d2e806541d29
Bruno <brunolegal3@gmail.com>
2026-08-05 20:05:12 -0300

    cpp03: ex01: noname constructor

--- cpp03/ex01/ScravTrap.cpp
+++ cpp03/ex01/ScravTrap.cpp
@@ -6,27 +6,39 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 18:44:56 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/05 19:45:51 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/05 20:04:54 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScravTrap.hpp"
 
-// ScravTrap::ScravTrap()
-// {}
+ScravTrap::ScravTrap():
+_guardMode(false)
+{
+	std::cout << "ScravTrap Default Constructor has been called\n";
+}
 
-// ScravTrap::ScravTrap( const ScravTrap &other )
-// {}
-
-// ScravTrap&	ScravTrap::operator=( const ScravTrap& other )
+// ScravTrap::ScravTrap( const ScravTrap &other ):
+// _guardMode(other._guardMode)
 // {
-// 	if (this == &other)
-// 		return *this;
-// 	return *this;
+// 	this.
+// 	std::cout << "ScravTrap Copy Constructor destructor has been called\n";
 // }
 
-// ScravTrap::~ScravTrap()
-// {}
+ScravTrap&	ScravTrap::operator=( const ScravTrap& other )
+{
+	std::cout << "ScravTrap Assign Operator has been called\n";
+	if (this == &other)
+		return *this;
+
+	_guardMode = other._guardMode;
+	return *this;
+}
+
+ScravTrap::~ScravTrap()
+{
+	std::cout << "ScravTrap Destructor has been called\n";
+}
 
 void	ScravTrap::guardGate( void )
 {
--- cpp03/ex01/ScravTrap.hpp
+++ cpp03/ex01/ScravTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 18:45:00 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/05 19:40:54 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/05 20:03:15 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ private:
 
 
 public:
-	void	guardGate( void );
+	ScravTrap();
+	// ScravTrap( const std::string name );
+	// ScravTrap( const ScravTrap &other );
+	ScravTrap& operator=( const ScravTrap &other );
+	~ScravTrap();
 
+	void	guardGate( void );
 };
 
 #endif
