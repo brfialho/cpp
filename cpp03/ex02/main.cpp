7a3e77fd4a34c085de03f5fe9e3498076f29866b
brfialho <brunolegal3@gmail.com>
2026-08-13 19:33:28 -0300

    cpp03: ex02: fixed ctrl D during high five

--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 17:49:03 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/06 17:15:40 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/13 19:28:41 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	main ( void )
 	ScravTrap 	s("SCRAV");
 
 	f.highFivesGuys();
+	if (std::cin.failbit)
+		return (0);
 	s.guardGate();
 	for (int i = 0; i < 52; i++)
 	{
--- cpp03/ex02/src/FragTrap.cpp
+++ cpp03/ex02/src/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: brfialho <brfialho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/06 16:10:23 by brfialho          #+#    #+#             */
-/*   Updated: 2026/08/06 17:19:26 by brfialho         ###   ########.fr       */
+/*   Updated: 2026/08/13 19:30:23 by brfialho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,11 @@ void	FragTrap::highFivesGuys(void)
 			<<	"Type Y to high five him or N to ignore"
 			<< RESET
 			<< std::endl;
-	std::getline(std::cin, input);
+	if (!std::getline(std::cin, input))
+	{
+		std::cin.setstate(std::ios::failbit);
+		return;
+	}
 	if (input == "Y" || input == "y")
 	{
 		std::cout << BOLD_GREEN
